95c54924b3e0a097ae6bc0d52b53b9e900d3bc8f
mahmoud zahran <mmzahran30@gmail.com>
2025-02-15 14:11:20 +0300

    edit user schema and hash the password in the db

--- Back-End/Models/user.model.ts
+++ Back-End/Models/user.model.ts
@@ -1,15 +1,54 @@
 import mongoose, { Schema, Document } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
-    name: string;
-    email: string;
-    password: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  role: string;
+  gender: string;
+  phone: number;
+  birthDate: Date;
 }
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: [true, "Name is required"] },
-    email: { type: String, required: [true, "Email is required"], unique: [true, "Email already exists"] },
-    password: { type: String, required: [true, "Password is required"] },
+  name: { type: String, required: [true, "Name is required"] },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email already exists"],
+    validate: [validator.isEmail, "Invalid email format"],
+  },
+  password: { 
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Confirm password is required"],
+    validate: {
+      validator: function (this: IUser, val: string) {
+        return val === this.password;
+      },
+    },
+  },
+  role: { type: String, required: [true, "Role is required"] },
+  gender: { type: String, required: [true, "Gender is required"] },
+  phone: { type: Number, required: [true, "Phone is required"] },
+  birthDate: { type: Date, required: [true, "Birth date is required"] },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined as unknown as string;
+  next();
 });
 
 
