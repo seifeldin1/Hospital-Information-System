7c6f199668fde5e1302a8c72d0aae7854c924294
karimyasser107 <karimyasser34@gmail.com>
2025-02-17 20:10:44 +0200

    reused endpoints controllers of view all doctors/patients

--- Back-End/Routes/doctor.routes.ts
+++ Back-End/Routes/doctor.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const doctorController = require('../Controllers/doctor.controller')
+const adminController =require('../Controllers/admin.controller');
 const router = express.Router();
 
 router.post("/" , doctorController.addDoctor)
@@ -7,5 +8,6 @@ router.get("/:doctorID" , doctorController.getDoctor)
 router.get("/" , doctorController.getAllDoctors)
 router.put("/:doctorID" , doctorController.editDoctorInfo)
 router.delete("/:doctorID" , doctorController.deleteDoctor)
+router.get('/view-all-patients',adminController.getAllPatients);
 
 export default router
